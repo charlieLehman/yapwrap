bb3ee173507561031e5da765256aca22a1b29fc5
Advait <advaitkoparkar@gmail.com>
2019-02-17 23:56:57 -0500

    Added alexnet for svhn. Val Acc ~67% for 10 Epochs.

--- yapwrap/models/__init__.py
+++ yapwrap/models/__init__.py
@@ -1,3 +1,4 @@
 from .tinyresnet import *
 from .complementconstraint import *
 from .mnist_convnet import *
+from .alexnet_svhn import *
---
+++ yapwrap/models/alexnet_svhn.py
@@ -0,0 +1,37 @@
+import torch
+import torchvision
+
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from copy import copy
+
+__all__ = ['ALEXNET_SVHN']
+
+class ALEXNET_SVHN(nn.Module):
+    def __init__(self, num_classes=10):
+        self.name = self.__class__.__name__
+        super(ALEXNET_SVHN, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=5),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(64, 192, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+        self.classifier = nn.Linear(256, num_classes)
+
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        return x
